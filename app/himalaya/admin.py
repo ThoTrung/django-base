6684862e29116abca29b84c751cada2e90c73ecb
thomac <tho.nt@gmail.com>
2023-04-27 21:47:19 +0700

    edit admin display

--- app/himalaya/admin.py
+++ app/himalaya/admin.py
@@ -3,10 +3,12 @@ from . import models
 
 # Register your models here.
 class DmDoituongAdmin(admin.ModelAdmin):
+    list_display = ["madoituong", "tendoituong", "dienthoai"]
     pass
 admin.site.register(models.DmDoituong,DmDoituongAdmin)
 
 
 class DmNhomdoituongAdmin(admin.ModelAdmin):
+    list_display = ["tennhom"]
     pass
 admin.site.register(models.DmNhomdoituong,DmNhomdoituongAdmin)
